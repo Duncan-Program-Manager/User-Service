85e544cf3c7d131d547c3e2c8dbd7f61ccad4f4e
Duncan Schoenmakers <400156@student.fontys.nl>
2022-06-07 15:09:31 +0200

    fixed broken import

--- src/main/java/com/twonow/springapi/BasicSpringApiApplication.java
+++ src/main/java/com/twonow/springapi/BasicSpringApiApplication.java
@@ -2,7 +2,6 @@ package com.twonow.springapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 public class BasicSpringApiApplication {
